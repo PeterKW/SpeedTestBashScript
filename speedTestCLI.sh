e756d15c1c8b3458c0d3bc51199b64aa204f0fa6
Peter <pkwjn1633@gmail.com>
2020-12-10 22:27:19 +0000

    Added code to enable file activation from any location

--- speedTestCLI.sh
+++ speedTestCLI.sh
@@ -34,6 +34,15 @@ if [ "" = "$PKG_OK" ]; then
   sudo apt-get install $REQUIRED_PKG
 fi
 
+#https://www.golinuxcloud.com/get-script-name-get-script-path-shell-script/
+script_name1=`basename $0`
+script_path1=$(dirname $(readlink -f $0))
+script_path_with_name="$script_path1/$script_name1"
+script_output_with_name="$script_path1/speedTestCLI.txt"
+
+echo "Script path with name: $script_path_with_name"
+echo "Script output with name: $script_output_with_name"
+
 #https://stackoverflow.com/a/15748003
 if [ $n =  0 ]; then
     echo 'How many times to run speed test? CTRL-C to cancel'
@@ -51,11 +60,11 @@ fi
 for (( c=1; c<=$n; c++ ))
 do
     echo $c/$n&&date
-    touch speedTestCLI.txt
-    date >> speedTestCLI.txt
-    speedtest-cli --simple >> speedTestCLI.txt
+    touch $script_output_with_name
+    date >> $script_output_with_name
+    speedtest-cli --simple >> $script_output_with_name
 done
 
 echo
 echo
-cat speedTestCLI.txt
+cat $script_output_with_name
